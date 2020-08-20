9b2f22b3d17f3e5b47dc3e31d3673707eda91fc2
Jordan Duggan <jordan.duggan@orchardrock.co.uk>
2020-08-20 15:29:35 +0100

    Init

---
+++ Project Gizmo/Gizmo V1.00/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gizmo_V1._00.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
