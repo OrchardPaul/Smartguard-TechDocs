757988b867bf6131d23594d9785fea6657db548a
Jordan Duggan <jordan.duggan@orchardrock.co.uk>
2020-08-25 12:37:27 +0100

    Gizmo resi database connected

---
+++ Project Gizmo/Gizmo V1_00/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gizmo_V1_00.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Project Gizmo/Gizmo V1_00/Data/OR_RESI_DATA_Service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gizmo_V1_00.Models;
+
+namespace Gizmo_V1_00.Data
+{
+    public class OR_RESI_DATA_Service
+    {
+
+        private readonly P4W_OR_RESI_V5_DEVContext _Context;
+
+        public OR_RESI_DATA_Service(P4W_OR_RESI_V5_DEVContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task<UsrOrResiMt[]> GetDataRows()
+        {
+            var dataRows = _Context.UsrOrResiMt.ToArray();
+
+            return await Task.FromResult(result: dataRows);
+        }
+
+    }
+}
