6a535b48c411da49b7ce010c880204e754493fa4
Jordan Duggan <jordan.duggan@orchardrock.co.uk>
2020-09-08 10:36:25 +0100

    CRUD updated

--- Project Gizmo/Gizmo V1_00/Data/OR_RESI_Chapters/OR_RESI_Chapters_Service.cs
+++ Project Gizmo/Gizmo V1_00/Data/OR_RESI_Chapters/OR_RESI_Chapters_Service.cs
@@ -9,11 +9,14 @@ namespace Gizmo_V1_00.Data.OR_RESI_Chapters
 {
     public interface IOR_RESI_Chapters_Service
     {
-        Task<List<UsrOrDefChapterManagement>> GetChapterListByCaseType(string Type, string caseType);
-        Task<List<UsrOrDefChapterManagement>> GetDocListByChapterAndDocType(String caseType, String chapter, String docType);
-        Task<UsrOrDefChapterManagement> Add(UsrOrDefChapterManagement toDo);
+        Task<UsrOrDefChapterManagement> Add(UsrOrDefChapterManagement item);
         Task<UsrOrDefChapterManagement> Delete(int id);
-        Task<UsrOrDefChapterManagement> Update(UsrOrDefChapterManagement toDo);
+        Task<List<UsrOrDefChapterManagement>> GetChapterListByCaseType(string Type, string caseType);
+        Task<List<UsrOrDefChapterManagement>> GetDocListByChapter(string caseType, string chapter);
+        Task<List<UsrOrDefChapterManagement>> GetDocListByChapterAndDocType(string caseType, string chapter, string docType);
+        int GetParentId(String caseType, String chapter);
+        int? GetMaxSeqNum(int parentId);
+        Task<UsrOrDefChapterManagement> Update(UsrOrDefChapterManagement item);
     }
 
     public class OR_RESI_Chapters_Service : IOR_RESI_Chapters_Service
@@ -30,25 +33,67 @@ namespace Gizmo_V1_00.Data.OR_RESI_Chapters
             var Chapters = await _context.UsrOrDefChapterManagement
                 .Where(C => C.Type == Type)
                 .Where(C => C.CaseType == caseType)
+                .OrderBy(C => C.SeqNo)
                 .ToListAsync();
             return Chapters;
         }
 
-        public async Task<List<UsrOrDefChapterManagement>> GetDocListByChapterAndDocType(String caseType, String chapter, String docType)
+        public async Task<List<UsrOrDefChapterManagement>> GetDocListByChapter(String caseType, String chapter)
         {
-            //List<string> Doctype = new List<string>() { "Doc", "Letter", "Form", "Step" };
+            List<string> Doctype = new List<string>() { "Doc", "Letter", "Form", "Step" };
 
             var idRecord = _context.UsrOrDefChapterManagement
                 .Where(C => C.Name == chapter)
                 .Where(C => C.CaseType == caseType)
                 .Where(C => C.Type == "Chapter")
                 .Single();
-            
+
+            return await _context.UsrOrDefChapterManagement
+                                .Where(C => C.ParentId == idRecord.Id)
+                                .Where(C => Doctype.Contains(C.Type))
+                                .ToListAsync();
+        }
+
+        public async Task<List<UsrOrDefChapterManagement>> GetDocListByChapterAndDocType(String caseType, String chapter, String docType)
+        {
+
+            var idRecord = _context.UsrOrDefChapterManagement
+                .Where(C => C.Name == chapter)
+                .Where(C => C.CaseType == caseType)
+                .Where(C => C.Type == "Chapter")
+                .Single();
+
             return await _context.UsrOrDefChapterManagement
                                 .Where(C => C.ParentId == idRecord.Id)
                                 .Where(C => C.Type == docType)
-                                //.Where(C => Doctype.Contains(C.Type))
                                 .ToListAsync();
+        }
+
+        public int GetParentId(String caseType, String chapter)
+        {
+
+            int idRecord =  _context.UsrOrDefChapterManagement
+                .Where(C => C.Name == chapter)
+                .Where(C => C.CaseType == caseType)
+                .Where(C => C.Type == "Chapter")
+                .Select(C=> C.Id)
+                .Single();
+
+            return idRecord;
+        }
+
+        public int? GetMaxSeqNum(int parentId)
+        {
+            int? seqRecord = _context.UsrOrDefChapterManagement
+                .Where(C => C.ParentId == parentId)
+                .Max(C => C.SeqNo);
+
+            if (seqRecord is null)
+                seqRecord = 0;
+            else
+                seqRecord++;
+
+            return seqRecord;
         }
 
 
