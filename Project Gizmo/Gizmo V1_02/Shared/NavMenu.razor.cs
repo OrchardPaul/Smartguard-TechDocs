4db4411a70f388e098a000753f4f3eb6643db1a6
Paul Taylor <paul.taylor@orchardrock.co.uk>
2021-02-02 18:51:55 +0000

    Design changes for mobile.  NavMenu especially.

--- Project Gizmo/Gizmo V1_02/Shared/NavMenu.razor.cs
+++ Project Gizmo/Gizmo V1_02/Shared/NavMenu.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using Gizmo_V1_02.Services.SessionState;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
@@ -9,11 +10,19 @@ namespace Gizmo_V1_02.Shared
 {
     public partial class NavMenu
     {
+        [Inject]
+        public IUserSessionState userSession { get; set; }
+
+        [Inject]
+        public NavigationManager navigationManager { get; set; }
 
         private bool collapseNavMenu = true;
 
         private string NavMenuCssClass => collapseNavMenu ? "collapse" : null;
 
+        public string ModalInfoHeader { get; set; }
+        public string ModalHeight { get; set; }
+        public string ModalWidth { get; set; }
 
         private async Task ToggleNavMenu()
         {
@@ -27,5 +36,21 @@ namespace Gizmo_V1_02.Shared
             await jsRuntime.InvokeAsync<object>("HideAll");
         }
 
+        private void PrepareModalDelete(string modalHeader
+                                        , string modalHeight
+                                        , string modalWidth)
+        {
+            ModalInfoHeader = modalHeader;
+            ModalHeight = modalHeight;
+            ModalWidth = modalWidth;
+        }
+
+
+        public void NavigateToUserProfile()
+        {
+            //set Return URI
+            userSession.SetUserProfileReturnURI(navigationManager.Uri);
+            navigationManager.NavigateTo("/userprofile");
+        }
     }
 }
